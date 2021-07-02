9c3e612414ceaa1c03cfaa5429a22f4ca3518d87
FHU-yezi <yehaowei20060411@qq.com>
2021-07-02 11:20:34 +0800

    feat：完成基础数据获取部分的编写

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ if TOTAL_DATA_COUNT % 20 != 0:
     raise Exception("总数据量必须是 20 的整数倍")
 DATABASE_NAME = "UserData.db"  # 数据库名称
 SIMPLE_DATA_REQUESTS_COUNT = int(TOTAL_DATA_COUNT / 20)
-SLEEP_TIME = 0
+SLEEP_TIME = 0.03
+REQUESTS_BEFORE_SAVE = 10  # 多少次数据保存一次
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from time import sleep
 
 import colorama
+import requests
 from JianshuResearchTools.objects import User
 from JianshuResearchTools.rank import GetAssetsRankData
+from lxml import etree
 
 from config import *
 from db_config import *
@@ -29,10 +31,8 @@ db.create_tables([UserData])
 print_green("创建表成功")
 
 def StoreDataListToDatabase(db, data_list):
-    for item in data_list:  # 临时处理
-        item["avatar_url"] = item["name"]
+    for item in data_list:
         item["FP_count"] = item["FP"]
-        del item["name"]
         del item["FP"]
     UserData.insert_many(data_list).execute()
 
@@ -40,18 +40,36 @@ def BasicDataGetter(start_id):
     data = GetAssetsRankData(start_id)
     return data
 
+def FullDataGetter(basic_data):
+    result = basic_data
+    result["url"] = "".join(["https://www.jianshu.com/u/"), basic_data["uslug"]])
+    
+
 
 def BasicDataGettingProcess():
     print_green("开始获取基础数据")
     print("共需请求", SIMPLE_DATA_REQUESTS_COUNT, "次")
     if SLEEP_TIME == 0:
         print_yellow("请注意，不设置请求间隔可能被封禁")
+    data_list = []
+    requests_after_save = 0
+    start_id_when_save = 1
     for requests_count in range(SIMPLE_DATA_REQUESTS_COUNT):
         start_id = requests_count * 20 + 1  # JRT 是给人用的所以对起始值做了处理，但是这是程序
-        data_list = BasicDataGetter(start_id)
-        StoreDataListToDatabase(db, data_list)
-        print_green(f"{start_id} 至 {start_id + 20} 的数据已成功保存到数据库")
+        request_data_list = BasicDataGetter(start_id)
+        print(f"{start_id - 1} 至 {start_id + 20 - 1} 的数据获取成功")
+        requests_after_save += 1
+        data_list = data_list + request_data_list
+        if requests_after_save == REQUESTS_BEFORE_SAVE:
+            StoreDataListToDatabase(db, data_list)
+            print_green(f"{start_id_when_save - 1} 至 {start_id_when_save + len(data_list) - 1} 的数据已成功保存")
+            data_list = []
+            start_id_when_save = start_id + 20
+            requests_after_save = 0
         sleep(SLEEP_TIME)
+    print_green("全部任务完成！")
+    db.close()
+    print_green("数据库已正常关闭！")
     
     
 def FullDataGettingProcess():
